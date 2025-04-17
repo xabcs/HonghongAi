64e6e623edb609ea0c81a896affa260a8a46e0f3
方忠朋 <992565438@qq.com>
2025-04-17 11:49:35 +0800

    feat(app): 添加原谅值过低警告功能
    - 新增 warningMessage 状态用于显示警告信息
    - 在调整原谅值时，根据值的范围设置不同的警告消息
    - 在页面上添加固定位置的警告提示框
    - 优化用户体验，增加游戏互动性

--- app/page.tsx
+++ app/page.tsx
@@ -159,8 +159,19 @@ export default function Chat() {
 
       {/* 添加警告消息显示 */}
       {warningMessage && (
-        <div className="fixed top-20 left-1/2 transform -translate-x-1/2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative z-50 animate-bounce">
-          <span className="block sm:inline">{warningMessage}</span>
+        <div className="fixed top-24 left-1/2 transform -translate-x-1/2 w-full max-w-md mx-auto px-4">
+          <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg shadow-lg relative z-50 animate-bounce">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <p className="text-sm font-medium">{warningMessage}</p>
+              </div>
+            </div>
+          </div>
         </div>
       )}
 
